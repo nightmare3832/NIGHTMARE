0fbf46e10b806c19fd37e5667ec8e05b9509cee5
nightmare3832 <nightmare3832@yahoo.co.jp>
2015-11-26 19:31:38 +0900

    New Sound

--- src/pocketmine/network/protocol/LevelEventPacket.php
+++ src/pocketmine/network/protocol/LevelEventPacket.php
@@ -41,6 +41,7 @@ class LevelEventPacket extends DataPacket{
 	const EVENT_SOUND_SHOOT = 1002;
 	const EVENT_SOUND_DOOR = 1003;
 	const EVENT_SOUND_FIZZ = 1004;
+	const EVENT_SOUND_TNT_FILE = 1005;
 
 	const EVENT_SOUND_GHAST = 1007;
 	const EVENT_SOUND_GHAST_SHOOT = 1008;
