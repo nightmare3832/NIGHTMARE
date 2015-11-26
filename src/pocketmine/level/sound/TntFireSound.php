586a45cd57f2e3d3da54a930cdeee7a26e4af271
nightmare3832 <nightmare3832@yahoo.co.jp>
2015-11-26 19:40:59 +0900

    New Sound

--- src/pocketmine/level/sound/TntFireSound.php
+++ src/pocketmine/level/sound/TntFireSound.php
@@ -24,8 +24,8 @@ namespace pocketmine\level\sound;
 use pocketmine\math\Vector3;
 use pocketmine\network\protocol\LevelEventPacket;
 
-class ZombieHealSound extends GenericSound{
+class TntFireSound extends GenericSound{
 	public function __construct(Vector3 $pos, $pitch = 0){
-		parent::__construct($pos, LevelEventPacket::EVENT_SOUND_ZOMBIE_HEAL, $pitch);
+		parent::__construct($pos, LevelEventPacket::EVENT_SOUND_TNT_FIRE, $pitch);
 	}
 }
