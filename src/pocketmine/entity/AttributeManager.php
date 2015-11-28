fa439d9c4749ab4f8d762fa428d2f64231268bb7
nightmare3832 <nightmare3832@yahoo.co.jp>
2015-11-28 16:08:33 +0900

    speed

--- src/pocketmine/entity/AttributeManager.php
+++ src/pocketmine/entity/AttributeManager.php
@@ -31,6 +31,7 @@ class AttributeManager{
 	const MAX_HUNGER = 1;
 	const EXPERIENCE = 2;
 	const EXPERIENCE_LEVEL = 3;
+	const SPEED = 4;
 	/** @var Attribute[] */
 	protected $attributes = [];
 	/** @var Player */
@@ -45,6 +46,7 @@ class AttributeManager{
 		self::addAttribute(self::MAX_HUNGER, "player.hunger", 0, 20, 20, true);
 		self::addAttribute(self::EXPERIENCE, "player.experience", 0, 1, 0, true);
 		self::addAttribute(self::EXPERIENCE_LEVEL, "player.level", 0, 24791, 0, true);
+		self::addAttribute(self::SPEED, "generic.movementSpeed", 0, 1.7*pow(10,308), 0.7, true);
 	}
 
 	public function getPlayer() {
