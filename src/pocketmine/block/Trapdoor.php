f2fab3b16b7e0782d5b740142cbe33c1826c180c
nightmare3832 <nightmare3832@yahoo.co.jp>
2015-11-29 10:17:23 +0900

    updata

--- src/pocketmine/block/Trapdoor.php
+++ src/pocketmine/block/Trapdoor.php
@@ -20,38 +20,28 @@
 */
 
 namespace pocketmine\block;
-
 use pocketmine\item\Item;
 use pocketmine\item\Tool;
+use pocketmine\level\sound\DoorSound;
 use pocketmine\math\AxisAlignedBB;
 use pocketmine\Player;
-
 class Trapdoor extends Transparent{
-
 	protected $id = self::TRAPDOOR;
-
 	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
-
 	public function getName(){
 		return "Wooden Trapdoor";
 	}
-
 	public function getHardness(){
 		return 3;
 	}
-
 	public function canBeActivated(){
 		return true;
 	}
-
 	protected function recalculateBoundingBox(){
-
 		$damage = $this->getDamage();
-
 		$f = 0.1875;
-
 		if(($damage & 0x08) > 0){
 			$bb = new AxisAlignedBB(
 				$this->x,
@@ -71,7 +61,6 @@ class Trapdoor extends Transparent{
 				$this->z + 1
 			);
 		}
-
 		if(($damage & 0x04) > 0){
 			if(($damage & 0x03) === 0){
 				$bb->setBounds(
@@ -113,10 +102,8 @@ class Trapdoor extends Transparent{
 				);
 			}
 		}
-
 		return $bb;
 	}
-
 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
 		if(($target->isTransparent() === false or $target->getId() === self::SLAB) and $face !== 0 and $face !== 1){
 			$faces = [
@@ -130,26 +117,21 @@ class Trapdoor extends Transparent{
 				$this->meta |= 0x08;
 			}
 			$this->getLevel()->setBlock($block, $this, true, true);
-
 			return true;
 		}
-
 		return false;
 	}
-
 	public function getDrops(Item $item){
 		return [
 			[$this->id, 0, 1],
 		];
 	}
-
 	public function onActivate(Item $item, Player $player = null){
 		$this->meta ^= 0x04;
 		$this->getLevel()->setBlock($this, $this, true);
 		$this->level->addSound(new DoorSound($this));
 		return true;
 	}
-
 	public function getToolType(){
 		return Tool::TYPE_AXE;
 	}
