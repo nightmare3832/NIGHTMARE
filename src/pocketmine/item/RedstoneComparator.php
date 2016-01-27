871cce457077edf72bf827cee28fbd5d5b1a94ee
nightmare3832 <nightmare3832@yahoo.co.jp>
2016-01-27 21:29:50 +0900

    0.14.0

--- src/pocketmine/item/RedstoneComparator.php
+++ src/pocketmine/item/RedstoneComparator.php
@@ -7,7 +7,7 @@ use pocketmine\block\Block;
 class RedstoneComparator extends Item{
 	public function __construct($meta = 0, $count = 1){
 		$this->block = Block::get(Block::UNPOWERED_COMPARATOR);
-		parent::__construct(self::REDSTONE_REPEATER, $meta, $count, "Redstone Comparator");
+		parent::__construct(self::REDSTONE_COMPARATOR, $meta, $count, "Redstone Comparator");
 	}
 
 }
